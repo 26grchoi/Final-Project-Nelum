89fa9e59341136576a6b4c7de0fb03895a03b3f4
Grace Choi <grace202630@gmail.com>
2024-06-24 11:10:21 -0700

    Initial Commit

---
+++ Final Project: Nelum/Final_Project__NelumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project__NelumApp.swift
+//  Final Project: Nelum
+//
+//  Created by Grace Choi on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project__NelumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
